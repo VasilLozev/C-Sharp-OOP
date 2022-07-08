806959efc2dbbd22ccba06d37eb8f3e60ef595d4
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-07-08 13:30:08 +0300

    Add files via upload

---
+++ Heroes/Knight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Knight : Hero
+    {
+        public Knight(string name, int health, int armour) : base(name, health, armour)
+        {
+            name = Name;
+            health = Health;
+            armour = Armour;
+        }
+    }
+}
