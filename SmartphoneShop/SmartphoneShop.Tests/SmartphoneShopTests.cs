61e90f2bcc6d7052efa39de4a6350b4cd62caff1
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-12-19 00:18:22 +0200

    Add files via upload

---
+++ SmartphoneShop/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SmartphoneShop.Tests
+{
+    [TestFixture]
+    public class SmartphoneShopTests
+    {
+        private Smartphone smartPhone;
+        private Shop shop;
+        private string modelName = "IPhone 13 Pro Max";
+        private int maximumCharge = 100;
+        private int capacity = 3;
+       
+        [SetUp]
+        public void SetUp()
+        {
+            smartPhone = new Smartphone(modelName, maximumCharge);
+            shop = new Shop(capacity);
+        }
+        [Test]
+        public void Test_Constructor()
+        {
+            Assert.AreEqual(capacity, shop.Capacity);
+            Assert.AreEqual(0, shop.Count);
+        }
+        [Test]
+        public void Test_NegativeCapacityShouldThrow()
+        {
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                shop = new Shop(-5);
+            });
+        }
+
+        [Test]
+        public void Test_AddSmartPhoneShouldWork()
+        {
+            shop.Add(smartPhone);
+            
+            Assert.AreEqual(1, shop.Count);
+        }
+
+        [Test]
+        public void Test_DuplicateSmartphonesShouldNotBeAllowed()
+        {
+            shop.Add(smartPhone);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Add(smartPhone);
+            });
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Add(new Smartphone(modelName, 10000));
+            });
+        }
+
+        [Test]
+        public void Test_PhoneCannotBeAdded_WhenCapacityIsFull()
+        {
+            shop.Add(new Smartphone("1", 1));
+            shop.Add(new Smartphone("2", 1));
+            shop.Add(new Smartphone("3", 1));
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Add(new Smartphone("4", 1));
+            });
+        }
+
+        [Test]
+        public void Test_RemoveSmartPhoneShouldWork()
+        {
+            shop.Add(smartPhone);
+            shop.Remove(modelName);
+
+            Assert.AreEqual(0, shop.Count);
+        }
+
+        [Test]
+        public void Test_RemoveShouldThrow_WhenPhoneIsNotFound()
+        {
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.Remove("Non Existent");
+            });
+        }
+        [Test]
+        public void Test_TestPhoneShouldReduceBatteryCharge()
+        {
+            shop.Add(smartPhone);
+            shop.TestPhone(modelName, 50);
+
+            Assert.AreEqual(maximumCharge - 50, smartPhone.CurrentBateryCharge);
+           
+        }
+        [Test]
+        public void Test_TestPhoneShouldThrow_WhenPhoneIsNotExistent()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.TestPhone("Non Existant", 10);
+            });
+
+        }
+        [Test]
+        public void TestPhoneShouldNotWorkOnUnchargedPhones()
+        {
+            shop.Add(smartPhone);
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.TestPhone(modelName, maximumCharge+1);
+            });
+        }
+        [Test]
+        public void Test_ChargePhoneShouldThrow_WhenPhoneIsNotExistent()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                shop.ChargePhone("Non Existant");
+            });
+        }
+        [Test]
+        public void Test_ChargePhoneShouldSetBatteryToMax()
+        {
+            shop.Add(smartPhone);
+
+            shop.TestPhone(modelName, 50);
+
+            shop.ChargePhone(modelName);
+
+            Assert.AreEqual(maximumCharge, smartPhone.CurrentBateryCharge);
+        }
+    }
+}
