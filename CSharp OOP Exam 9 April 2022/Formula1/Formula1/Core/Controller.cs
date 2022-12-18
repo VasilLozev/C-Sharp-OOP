d97277859e720f7adbed6f81cdd41073269b6022
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2022-12-18 16:32:54 +0200

    Add files via upload

---
+++ CSharp OOP Exam 9 April 2022/Formula1/Formula1/Core/Controller.cs
@@ -0,0 +1,170 @@
+﻿using Formula1.Core.Contracts;
+using Formula1.Models;
+using Formula1.Models.Contracts;
+using Formula1.Repositories;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
+
+namespace Formula1.Core
+{
+    public class Controller : IController
+    {
+        public Controller()
+        {
+            pilotRepository = new PilotRepository();
+            raceRepository = new RaceRepository();
+            carRepository = new FormulaOneCarRepository();
+        }
+        private PilotRepository pilotRepository;
+        private RaceRepository raceRepository;
+        private FormulaOneCarRepository carRepository;
+
+
+        public string AddCarToPilot(string pilotName, string carModel)
+        {
+            IPilot pilot = pilotRepository.FindByName(pilotName);
+            IFormulaOneCar car = carRepository.FindByName(carModel);
+            if (pilot == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotDoesNotExistOrHasCarErrorMessage, pilotName));
+            }
+            if (pilot.CanRace == true)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotDoesNotExistOrHasCarErrorMessage, pilotName));
+            }
+            if (car == null)
+            {
+                throw new NullReferenceException(string.Format(ExceptionMessages.CarDoesNotExistErrorMessage, carModel));
+            }
+            pilot.AddCar(car);
+            carRepository.Remove(car);
+            return string.Format(OutputMessages.SuccessfullyPilotToCar, pilotName, car.GetType().Name, carModel);
+        }
+
+        public string AddPilotToRace(string raceName, string pilotFullName)
+        {
+            IPilot pilot = pilotRepository.FindByName(pilotFullName);
+            IRace race = raceRepository.FindByName(raceName);
+            if (race == null)
+            {
+                throw new NullReferenceException(String.Format(ExceptionMessages.RaceDoesNotExistErrorMessage, raceName));
+            }
+            if (pilot == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotDoesNotExistErrorMessage, pilotFullName));
+            }
+            if (race.Pilots.Any(x=>x.FullName == pilotFullName))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotDoesNotExistErrorMessage, pilotFullName));
+            }
+            if (!pilot.CanRace)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotDoesNotExistErrorMessage, pilotFullName));
+            }
+            race.AddPilot(pilot);
+            return String.Format(OutputMessages.SuccessfullyAddPilotToRace, pilotFullName, raceName);
+        }
+
+        public string CreateCar(string type, string model, int horsepower, double engineDisplacement)
+        {
+            if (carRepository.Models.Any(x => x.Model == model))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.CarExistErrorMessage, model));
+            }
+            if (type != "Ferrari" && type != "Williams")
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InvalidTypeCar, type));
+            }
+            IFormulaOneCar car = null;
+            if (type == "Ferrari")
+            {
+                car = new Ferrari (model, horsepower, engineDisplacement);
+            }
+            if (type == "Williams")
+            {
+                car = new Williams(model, horsepower, engineDisplacement);
+            }
+            carRepository.Add(car);
+            return string.Format(OutputMessages.SuccessfullyCreateCar, type, model);
+        }
+
+        public string CreatePilot(string fullName)
+        {
+            if (pilotRepository.Models.Any(x=>x.FullName == fullName))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.PilotExistErrorMessage, fullName));
+            }
+            IPilot pilot = new Pilot(fullName);
+            pilotRepository.Add(pilot);
+            return String.Format(OutputMessages.SuccessfullyCreatePilot, fullName);
+        }
+
+        public string CreateRace(string raceName, int numberOfLaps)
+        {
+            if (raceRepository.Models.Any(x=>x.RaceName == raceName))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceExistErrorMessage, raceName));
+            }
+            IRace race = new Race(raceName, numberOfLaps);
+            raceRepository.Add(race);
+            return String.Format(OutputMessages.SuccessfullyCreateRace, raceName);
+
+        }
+
+        public string PilotReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in pilotRepository.Models.OrderByDescending(x => x.NumberOfWins))
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RaceReport()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in raceRepository.Models)
+            {
+                if (item.TookPlace == true)
+                {
+                    sb.AppendLine(item.RaceInfo());
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string StartRace(string raceName)
+        {
+            IRace race = raceRepository.FindByName(raceName);
+
+            if (race == null)
+            {
+                throw new NullReferenceException(String.Format(ExceptionMessages.RaceDoesNotExistErrorMessage,raceName));
+            }
+            if (race.Pilots.Count < 3)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InvalidRaceParticipants, raceName));
+            }
+            if (race.TookPlace == true)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.RaceTookPlaceErrorMessage, raceName));
+            }
+
+            List<IPilot> orderedPilots = race.Pilots.OrderByDescending(x=>x.Car.RaceScoreCalculator(race.NumberOfLaps)).ToList();
+
+            race.TookPlace = true;
+            orderedPilots.First().WinRace();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Pilot {orderedPilots.First().FullName} wins the { raceName } race.");
+            sb.AppendLine($"Pilot {orderedPilots[1].FullName} is second in the {raceName} race.");
+            sb.AppendLine($"Pilot {orderedPilots[2].FullName} is third in the {raceName} race.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CSharp OOP Exam 9 April 2022/Formula1/Formula1/Models/Race.cs
@@ -0,0 +1,87 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        public string RaceName
+        {
+            get
+            {
+                return _raceName;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidRaceName, value));
+                }
+                _raceName = value;
+            }
+        }
+        private string _raceName;
+        public int NumberOfLaps
+        {
+            get 
+            {
+                
+                return _numberOfLaps;
+            }
+            private set {
+                if (value < 1)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidLapNumbers, value));
+                }
+                _numberOfLaps = value; }
+        }
+        private int _numberOfLaps;
+
+        private bool tookPlace;
+
+        public bool TookPlace
+        {
+            get { return tookPlace; }
+            set { tookPlace = value; }
+        }
+
+        private ICollection<IPilot> pilots;
+
+        public ICollection<IPilot> Pilots
+        {
+            get { return pilots; }
+        }
+
+
+        public void AddPilot(IPilot pilot)
+        {
+            Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {Pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");
+            if (TookPlace == true)
+            {
+                sb.AppendLine($"Took place: Yes");
+            }
+            else
+            {
+                sb.AppendLine($"Took place: No");
+            }
+            return sb.ToString().Trim();
+        }
+        public Race(string raceName, int numberOfLaps)
+        {
+            pilots = new List<IPilot>();
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+        }
+    }
+}
